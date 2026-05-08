8eaf230a15f23768e5217ee910145b3f3a17b8f0
Giselle Georges <giselle.georges@nextintl.com>
2026-05-08 15:33:46 -0400

    back

--- client/e-commerce-frontend/src/app/products/page.tsx
+++ client/e-commerce-frontend/src/app/products/page.tsx
@@ -1,5 +1,6 @@
 
 import ProductList from "@/src/components/ProductList";
+import { Suspense } from "react";
 
 
 const ProductsPage = async ({
@@ -10,7 +11,9 @@ const ProductsPage = async ({
   
   return (
     <div className="">
+       <Suspense fallback={<div>Loading search...</div>}>
       <ProductList  params="products"/>
+      </Suspense>
     </div>
   );
 };
