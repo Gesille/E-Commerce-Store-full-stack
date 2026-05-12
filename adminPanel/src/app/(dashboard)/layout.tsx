93b6f7cbd3561fe4294ea111eaea20a4c808b328
Giselle Georges <giselle.georges@nextintl.com>
2026-05-12 13:42:14 -0400

    POS page

--- adminPanel/src/app/(dashboard)/layout.tsx
+++ adminPanel/src/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
 import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
