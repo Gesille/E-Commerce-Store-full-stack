59ba57ee4f1efc6df7f32cedd7c14ec4b3cde200
Giselle Georges <giselle.georges@nextintl.com>
2026-05-22 16:02:20 -0400

    print

--- server/routes/receipts.route.ts
+++ server/routes/receipts.route.ts
@@ -15,7 +15,7 @@ const receiptRouter = express.Router();
 // ─────────────────────────────────────────────────────────
 
 receiptRouter.get(
-  "/get-all-receipts-all",
+  "/get-all-receipts",
   isAuthenticated,
   authorizeRoles("admin", "cashier"),
   getReceipts
