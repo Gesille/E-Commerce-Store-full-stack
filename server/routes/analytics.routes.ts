06f05e7bfa723aa10d4da3f0c48a2c7e14c06f4e
Giselle Georges <giselle.georges@nextintl.com>
2026-05-22 10:58:57 -0400

    dashboard pos

--- server/routes/analytics.routes.ts
+++ server/routes/analytics.routes.ts
@@ -48,7 +48,7 @@ AnalyticsRouter.get(
 );
 
 AnalyticsRouter.get(
-  "/payment-methods",
+  "/payment-methods/split",
   isAuthenticated,
   authorizeRoles("admin", "cashier"),
   getPaymentMethodsSplit,
