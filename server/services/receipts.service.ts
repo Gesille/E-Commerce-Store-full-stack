f87aa533c04a5f788cae68ba171b4fd64c0e6586
Giselle Georges <giselle@nextintl.com>
2026-06-23 11:24:18 -0400

    return policey

--- server/services/receipts.service.ts
+++ server/services/receipts.service.ts
@@ -274,7 +274,8 @@ const templateData = {
   shopABST:    process.env.SHOP_ABST    ?? "0161466",
   shopEmail:   process.env.SHOP_EMAIL   ?? "",
   logoUrl:     process.env.SHOP_LOGO_URL ?? "",
-  policyUrl: `${process.env.CLIENT_URL}/policy`,
+   policyUrl:
+    process.env.CLIENT_URL + "/policy",
 };
 
   
